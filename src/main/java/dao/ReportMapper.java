9cc995086f0737c15f10d5068fb0c8520136ec5b
liliang1617101@163.com <liliang1617101@163.com>
2020-10-01 16:31:26 +0800

    修改了report的dao的方法名，增加根据报告id下载功能

--- src/main/java/dao/ReportMapper.java
+++ src/main/java/dao/ReportMapper.java
@@ -6,17 +6,20 @@ import java.util.List;
 
 public interface ReportMapper {
     //查询所有的报告
-    List<Report> queryAllMaterials();
+    List<Report> queryAllReports();
 
     //上传一份报告
-    int UploadAMaterial(Report report);
+    int UploadAReport(Report report);
 
     //删除一份报告,根据报告id删除
-    int deleteMaterial(int ReportId);
+    int deleteReport(int ReportId);
 
     //根据文件名进行一次模糊查询,Title就是文件名
-    List<Report> queryMaterialByWord(String word);
+    List<Report> queryReportByWord(String word);
 
     //根据队伍的id查询报告
-    List<Report> queryMaterialsByTeamID(int TeamID);
+    List<Report> queryReportsByTeamID(int TeamID);
+
+    //根据报告id进行查询，主要用于下载时侯
+    Report queryReportByID(int ReportID);
 }
