261a84af775e378de00b7b9cd9f3be616a2cc6e4
liliang1617101@163.com <liliang1617101@163.com>
2020-10-01 16:46:03 +0800

    对filecontroller进行完善，接下来就可以进行具体事务的编写

--- src/main/java/controller/FileController.java
+++ src/main/java/controller/FileController.java
@@ -1,15 +1,21 @@
 package controller;
 
+import dao.ReportMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import service.MaterialService;
+import service.ReportService;
 
 @Controller
 @RequestMapping("/file")
-public class FileController {
+public class FileController {//对所有文件操作相关的进行管理
     @Autowired
     @Qualifier("materialServiceImpl")
-    private MaterialService materialService;
+    private MaterialService materialService;//对资料的管理
+    @Qualifier("reportServiceImpl")
+    private ReportService reportService;//对报告管理
+    
+
 }
