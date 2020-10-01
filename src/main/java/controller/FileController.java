67849b72069dde3ddfa3526c1bbd49f0f1e6018a
liliang1617101@163.com <liliang1617101@163.com>
2020-10-01 20:04:21 +0800

    完善了文件的controller层，以及实现了上传文件的功能和前端页面

--- src/main/java/controller/FileController.java
+++ src/main/java/controller/FileController.java
@@ -4,9 +4,15 @@ import dao.ReportMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import service.MaterialService;
 import service.ReportService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
 
 @Controller
 @RequestMapping("/file")
@@ -20,5 +26,38 @@ public class FileController {//对所有文件操作相关的进行管理
     @RequestMapping("/goToUploadMaterial")
     public String goToUpload(){
         return "uploadMaterial";
+    }
+
+    @RequestMapping("/uploadMaterial")
+    public String uploadMaterial(@RequestParam("file") CommonsMultipartFile file, HttpServletRequest request, Model model) throws IOException {
+        String uploadFileName = file.getOriginalFilename();
+        if("".equals(uploadFileName)){
+            return "redirect:/file/goToUploadMaterial";//返回到上传文件页面
+        }
+        System.out.println("uploadFileName:"+uploadFileName);
+
+        String path = request.getServletContext().getRealPath("/upload");
+
+        File realPath = new File(path);
+        if(!realPath.exists()){
+            realPath.mkdir();
+        }
+        System.out.println("保存地址："+realPath);
+
+        InputStream is = file.getInputStream();//文件输入流
+        OutputStream os = new FileOutputStream(new File(realPath,uploadFileName));//文件输出流
+
+        //读取写出
+        int len = 0;
+        byte[] buffer = new byte[1024];
+        while( (len = is.read(buffer)) != -1){
+            os.write(buffer,0,len);
+            os.flush();
+        }
+        os.close();
+        is.close();
+        System.out.println("上传成功");
+        model.addAttribute("uploadsuccess","上传成功");
+        return "forward:allBook";//继续解析
     }
 }
--- web/WEB-INF/jsp/uploadMaterial.jsp
+++ web/WEB-INF/jsp/uploadMaterial.jsp
@@ -2,8 +2,36 @@
 <html>
 <head>
     <title>上传资料</title>
+    <link href="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<h1>上传资料</h1>
+<div class="container">
+    <div class="row clearfix">
+        <div class="col-md-12 column">
+            <div class="page-header">
+                <h1>
+                    <small>上传文件————参考资料</small>
+                </h1>
+            </div>
+        </div>
+    </div>
+    <div class="row clearfix">
+        <div class="col-md-5"></div>
+        <div class="col-md-2 column">
+            <form action="${pageContext.request.contextPath}/file/uploadMaterial" enctype="multipart/form-data" method="post">
+                <input type="file" name="file">
+                <input type="submit" value="上传" onclick="return uploadConfirm();">
+            </form>
+        </div>
+        <div class="col-md-5"></div>
+    </div>
+</div>
+<script>
+    function uploadConfirm(){
+        if(confirm("确认上传"))
+            return true;
+        return false;
+    }
+</script>
 </body>
 </html>
