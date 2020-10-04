9a10e201af380a21c7d9be1c44ca2a489555ae1a
liliang1617101@163.com <liliang1617101@163.com>
2020-10-04 19:28:38 +0800

    修改pojo层的Team，添加了方法的相关引用

--- src/main/java/pojo/Team.java
+++ src/main/java/pojo/Team.java
@@ -1,5 +1,12 @@
 package pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Team {
     private int TeamID;
     private int ProjectID;
