51e2dd4025f240889d6b2689266f752622e607c7
liliang1617101@163.com <liliang1617101@163.com>
2020-10-05 10:42:21 +0800

    添加了TeamMapper的许多方法

--- src/main/java/dao/TeamMapper.java
+++ src/main/java/dao/TeamMapper.java
@@ -1,32 +1,42 @@
 package dao;
 
+import org.apache.ibatis.annotations.Param;
 import pojo.Team;
 
 import java.util.List;
 
 public interface TeamMapper {
-    //查询所有的队伍
-    List<Team> queryAllMaterials();
-
     //添加一个队伍
     int addTeam(Team team);
 
-    //删除一个队伍，根据TeamID,TeamID是由数据库自动递增分配的，
-    //ProjectID现在的想法应该是101 I组这样的东西
-    int deleteTeam(int id);
+    //删除一个队伍，根据TeamID,TeamID是由数据库自动递增分配的
+    //ProjectID应该是101 I组这样的东西
+    int deleteTeam(int TeamID);
 
-    //根据文件名进行一次模糊查询
-    List<Team> queryMaterialByWord(String word);
+    //修改一个记录
+    int updateTeam(Team team);
 
-    //根据路径和文件名查询
-    Team queryMaterialByPathAndName(Team material);
+    //查询所有的队伍
+    List<Team> queryAllTeams();
 
-    //修改一个记录，其实只需要修改时间
-    int updateMaterial(Team material);
+    //根据成员的id进行查询
+    Team queryTeamByMemberID(int id);
+
+    //根据成员的姓名进行模糊查询，模糊！！！用like
+    List<Team> queryTeamByMemberName();
+
+    //根据项目名进行一次模糊查询，模糊！！！
+    List<Team> queryTeamByProjectName(String word);
+
+    //根据队伍id查询
+    Team queryMaterialByTeamID(int id);
+
+    //查询所有的没有通过的队伍
+    List<Team> queryTeamNopass();
+
+    //查询所有的通过的队伍
+    List<Team> queryTeamPass();
 
     //查询前十五条记录,后来改成了查询若干了，，
-    List<Team> query15Materials();//(@Param("start") int start,@Param("len") int len);
-
-    //根据id查询记录
-    Team queryMaterialByID(int id);
+    List<Team> query15Materials(@Param("start") int start, @Param("len") int len);
 }
