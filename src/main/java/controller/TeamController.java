34b3e619004d5e72bb8c50cb12ff988af8dcca4f
liliang1617101@163.com <liliang1617101@163.com>
2020-10-07 20:05:17 +0800

    实现了学生跳转到队伍管理页面的功能

--- src/main/java/controller/TeamController.java
+++ src/main/java/controller/TeamController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pojo.Team;
 import service.TeamService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/team")
@@ -12,4 +15,12 @@ public class TeamController {
     @Autowired
     @Qualifier("teamServiceImpl")
     private TeamService teamService;
+
+    //前往学生管理页面
+    @RequestMapping("/goToStuTeamManage")
+    public String goToStuTeamManage(){
+        List<Team> teams = teamService.queryAllTeams();
+        System.out.println(teams);
+        return "stuTeamManage";
+    }
 }
