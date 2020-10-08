d24cceddaf03f9c2c22f951b6c1c7ec8853974da
liliang1617101@163.com <liliang1617101@163.com>
2020-10-08 10:41:58 +0800

    添加了一些后台的提示信息

--- src/main/java/controller/TeamController.java
+++ src/main/java/controller/TeamController.java
@@ -28,21 +28,25 @@ public class TeamController {
         User user = (User)session.getAttribute("UserSession");
 
         if(user == null){
+            System.out.println("team/goToTeamManage:用户未登录");
             return "login";//未登录
         }
         if("teacher".equals(user.getUserType())){
             //教师，应当前往另一个页面
+            System.out.println("team/goToTeamManage:教师用户"+user.getUserID());
             List<Team> teams = teamService.queryTeamsByTeacherID(user.getUserID());//首先根据教师获取他名下的项目
             model.addAttribute("teams",teams);
             return "teaTeamManage";
         }
         if("student".equals(user.getUserType())||"teamleader".equals(user.getUserType())||"teammate".equals(user.getUserType())){
             //学生用户
+            System.out.println("team/goToTeamManage:学生用户"+user.getUserID());
             Team team = teamService.queryTeamByMemberID(user.getUserID());//根据用户id查询项目中是否存在
             model.addAttribute("teams",team);
         }
         else{
             //为管理员用户
+            System.out.println("team/goToTeamManage:管理员用户"+user.getUserID());
             List<Team> teams = teamService.queryAnyTeams(0, 15);//查15条返回
             model.addAttribute("teams",teams);
             return "adminiTeamManage";
