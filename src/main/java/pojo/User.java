ece34955859ccb20c3732b61753e6c3584b96ece
liliang1617101@163.com <liliang1617101@163.com>
2020-10-12 08:50:05 +0800

    对pojo层的User进行修改，添加了新的变量，allsession，用以存储所有的用户id和sessionid

--- src/main/java/pojo/User.java
+++ src/main/java/pojo/User.java
@@ -4,10 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashMap;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    static private HashMap<String,String> allsession = new HashMap<String, String>();//存储每一个userid对应的sessionid,key为userid，value为session
+    //使用静态变量，使得可以所有的对象都可以取到，避免在teamcontroller里无法取到
     private int ID;//数据库中的id
     private String UserID;//用户登录使用的id
     private String UserPassword;//用户密码
