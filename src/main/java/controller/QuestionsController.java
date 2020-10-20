abd1d1649556542c5de8f70d57fb6033102d1f50
liliang1617101@163.com <liliang1617101@163.com>
2020-10-20 09:26:34 +0800

    添加了学生前往查看自己提出的问题的后端代码

--- src/main/java/controller/QuestionsController.java
+++ src/main/java/controller/QuestionsController.java
@@ -4,8 +4,15 @@ package controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pojo.Questions;
+import pojo.User;
 import service.QuestionsService;
+import service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 @RequestMapping("/questions")
@@ -13,5 +20,25 @@ public class QuestionsController {
     @Autowired
     @Qualifier("questionsServiceImpl")
     private QuestionsService questionsService;
-    
+
+    @Autowired
+    @Qualifier("userServiceImpl")
+    private UserService userService;
+
+    //学生前往查看问题页面
+    @RequestMapping("/stuGoToQuestionsManage")
+    public String stuGoToQuestionsManage(Model model, HttpServletRequest request){
+        User user = (User) request.getSession().getAttribute("UserSession");
+        if(user == null){
+            return "login";
+        }
+        if("student".equals(user.getUserType()) || "teamleader".equals(user.getUserType()) || "teammate".equals(user.getUserType())){
+            //学生用户，只能看自己的问题,,,,吧
+            List<Questions> questions = questionsService.queryByUserID(user.getUserID());
+            model.addAttribute("questions",questions);
+        }else {
+            return "dontHavePermission";
+        }
+        return "questionsManage_stu";
+    }
 }
---
+++ web/WEB-INF/jsp/questionsManage_stu.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>学生问题查看页面</title>
+</head>
+<body>
+
+</body>
+</html>
